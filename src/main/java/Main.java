69ecd51f88346f2538c14920e5c94fa6af75b41c
Paweł Podlasin <p.podlasin@evoqai.com>
2022-05-08 13:09:32 +0200

    cd

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,25 +3,27 @@ import weka.classifiers.lazy.IBk;
 import weka.core.*;
 import weka.core.converters.ConverterUtils;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.*;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Main {
 
+    //zapuścić 5 razy zapisać w tabeli w wordzie wyliczyc średnia i na wykresie pokazać średnią
     public static void main(String[] args) throws Exception {
-        ManhattanDistance manDistance = new ManhattanDistance();
-        EuclideanDistance euDistance = new EuclideanDistance();
-        ChebyshevDistance cheDistance = new ChebyshevDistance();
         int basicValue = 16;
         for (int threadDevide = 1; threadDevide <= basicValue; threadDevide *= 2) {
             List<Thread> threads = new ArrayList<>();
+            List<Integer> bestsKList = new ArrayList<>();
+            List<Double> procentTrueList = new ArrayList<>();
+
             var startDate = new Date();
             for (int threadNumber = 1; threadNumber <= threadDevide; threadNumber++) {
                 int finalThreadDevide = threadDevide;
                 int finalThreadNumber = threadNumber;
                 Thread thread = new Thread(() -> {
+                    int bestK = 0;
+                    double bestProcent = 0;
                     try {
                         for (int kValue = ((basicValue / finalThreadDevide) * (finalThreadNumber - 1)) + 1; kValue <= ((basicValue / finalThreadDevide) * finalThreadNumber); kValue++) {
                             ConverterUtils.DataSource source = new ConverterUtils.DataSource("data/Dry_Bean_Dataset.arff");
@@ -30,24 +32,22 @@ public class Main {
                                 data.setClassIndex(data.numAttributes() - 1);
                             }
                             IBk classifier = new IBk();
-                            classifier.buildClassifier(data);
                             String[] options = Utils.splitOptions("-K " + kValue + " -W 0");
                             classifier.setOptions(options);
-                            classifier.setCrossValidate(false);
-                            classifier.setBatchSize("100");
-                            classifier.setDebug(false);
-                            classifier.setDoNotCheckCapabilities(false);
-                            classifier.setMeanSquared(false);
-                            classifier.getNearestNeighbourSearchAlgorithm().setDistanceFunction(manDistance);
-                            classifier.setNumDecimalPlaces(2);
-                            classifier.setWindowSize(0);
                             classifier.buildClassifier(data);
                             Evaluation eval = new Evaluation(data);
                             eval.crossValidateModel(classifier, data, 10, new Random(1));
+                            if(eval.weightedTruePositiveRate() >= bestProcent) {
+                                bestK = kValue;
+                                bestProcent = eval.weightedTruePositiveRate();
+                                System.out.println("New Best K = " + bestK);
+                            }
                         }
                     } catch (Exception e) {
                         System.err.println(e);
                     }
+                    bestsKList.add(bestK);
+                    procentTrueList.add(bestProcent);
                 });
                 threads.add(thread);
                 thread.start();
@@ -59,10 +59,20 @@ public class Main {
                     e.printStackTrace();
                 }
             }
+            int theBestKForTheBestK = 0;
+            double theBestProcentForTheBestProcent = 0.0;
+            for (int i = 0; i < procentTrueList.size(); i++) {
+                if(theBestProcentForTheBestProcent < procentTrueList.get(i)) {
+                    theBestKForTheBestK = bestsKList.get(i);
+                    theBestProcentForTheBestProcent = procentTrueList.get(i);
+                }
+            }
             var endDate = new Date();
             var miliSecond = Math.abs(endDate.getTime() - startDate.getTime());
-            System.out.println("Liczba threadów: " + threadDevide);
-            System.out.println("Czas: " + miliSecond/1000 +"s");
+            System.out.println("How many thread: " + threadDevide);
+            System.out.println("The best K = " + theBestKForTheBestK);
+            System.out.println("Time: " + miliSecond/1000 +"s");
         }
+
     }
 }
